2db8c9c4d40e83c94cd0d4d031e2a880f0f004f8
feng <ncuwff@126.com>
2017-07-28 18:41:50 +0800

    Hello,world!

---
+++ src/001_hello_world.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: 001_hello_world.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 28 Jul 2017 06:39:17 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello,world!"<<endl;
+}
